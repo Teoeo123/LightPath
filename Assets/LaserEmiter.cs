e796c54f352f35d46d08dee732a9183b2df56b26
Teoeo <michastedoroczyk@o2.pl>
2023-03-08 00:18:26 +0100

    laser reflection fix

--- Assets/LaserEmiter.cs
+++ Assets/LaserEmiter.cs
@@ -24,11 +24,12 @@ public class LaserEmiter : MonoBehaviour
         
         RaycastHit2D hitInfo = Physics2D.Raycast((Vector2)transform.position, transform.right, MaxRayDistance, LayerDetection);
         bool isMirror = false;
-        Debug.Log("1st reflection: " + Vector2.Reflect(-transform.right, hitInfo.normal));
-        Debug.Log("right vector:" + (Vector2)transform.right);
-        Vector2 mirrorHitPoint = -transform.right;
-        //test siema
+        Debug.Log("1st normal: " + hitInfo.normal);
+        Vector2 mirrorHitPoint = Vector2.zero;
         Vector2 mirrorHitNormal = Vector2.zero;
+        Vector2 mirrorLastHitPoint = (Vector2)transform.position;
+        Debug.Log("1st vector: " + (hitInfo.collider ? (hitInfo.point - mirrorLastHitPoint).normalized : "null" ));
+        Debug.Log("1st reflection: " + (hitInfo.collider ? Vector2.Reflect((hitInfo.point - mirrorLastHitPoint).normalized, hitInfo.normal) : "null"));
 
         for (int i = 0; i < reflections; i++)
         {
@@ -37,12 +38,12 @@ public class LaserEmiter : MonoBehaviour
             if (hitInfo.collider != null)
             {
                 LineOfSight.SetPosition(LineOfSight.positionCount - 1, hitInfo.point);
-                isMirror = false;
                 if (hitInfo.collider.CompareTag("Mirror"))
                 {
-                    hitInfo = Physics2D.Raycast((Vector2)hitInfo.point, Vector2.Reflect(mirrorHitPoint, hitInfo.normal), MaxRayDistance, LayerDetection);
-                    mirrorHitNormal = (Vector2)hitInfo.normal;
-                    mirrorHitPoint = (Vector2)hitInfo.point;
+                    mirrorHitPoint= hitInfo.point;
+                    mirrorHitNormal= hitInfo.normal;
+                    hitInfo = Physics2D.Raycast(mirrorHitPoint, Vector2.Reflect((mirrorHitPoint - mirrorLastHitPoint).normalized, mirrorHitNormal), MaxRayDistance, LayerDetection);
+                    if(hitInfo.collider != null) mirrorLastHitPoint= mirrorHitPoint;
                     isMirror = true;
                 }
                 else break;
@@ -51,7 +52,7 @@ public class LaserEmiter : MonoBehaviour
             {
                 if (isMirror)
                 {
-                    LineOfSight.SetPosition(LineOfSight.positionCount - 1, mirrorHitPoint + Vector2.Reflect(mirrorHitPoint, mirrorHitNormal) * MaxRayDistance);
+                    LineOfSight.SetPosition(LineOfSight.positionCount - 1, mirrorHitPoint + Vector2.Reflect((mirrorHitPoint - mirrorLastHitPoint).normalized,mirrorHitNormal) * MaxRayDistance );
                     break;
                 }
                 else
